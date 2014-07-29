9ee0be4708bd5a8f529067d0e8e224c88477e52f
Igor Zvyagintsev <inoy@orlec.ru>
2014-07-29 17:47:20 +0400

    мелкие правки

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-/*$(function() {
-  $( '#mi-slider' ).catslider();
-});*/
-
 $(".navclass a").click(function(){
   $(".dropdown-container").slideToggle();
 });
 
+(function($){
+  $(window).load(function(){
+    $("#dropdown-menu").mCustomScrollbar({
+      axis:"x",
+      scrollInertia:950,
+      contentTouchScroll:25,
+      theme:"light",        
+      advanced:{
+          autoExpandHorizontalScroll:true,
+          autoScrollOnFocus:"input,textarea,select,button,datalist,keygen,a[tabindex],area,object,[contenteditable='true']",
+          updateOnContentResize:true,
+          updateOnSelectorLength:true
+        }
+    });
+  });
+})(jQuery);
